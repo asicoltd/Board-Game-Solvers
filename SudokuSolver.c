deb6c5144ab8dbc9e10b07bb58cc275706fddfb3
MD. ASIF HASAN <61615468+asicoltd@users.noreply.github.com>
2025-02-20 23:17:21 +0600

    Update SudokuSolver.c
    Using recursion

--- SudokuSolver.c
+++ SudokuSolver.c
@@ -1,96 +1,64 @@
-int check(int sud[9][9]){
-    int i, j, k, l;
-    for( i = 0 ; i < 9 ; i++ ){
-        for( j = 0 ; j < 9 ; j++ ){
-            for( k = j ; k < 8 ; k++ ){
-               if( sud[i][j] == sud[i][k+1] && sud[i][j] != 0 || sud[i][j] > 9 ){
-                return 0;
-               }
-            }
-        }
+#include <stdio.h>
+
+int check(int sud[9][9], int row, int col, int num) {
+    // Check the row
+    for (int i = 0; i < 9; i++) {
+        if (sud[row][i] == num) return 0;
     }
-    for( i = 0 ; i < 9 ; i++ ){
-        for( j = 0 ; j < 9 ; j++ ){
-            for( k = j ; k < 8 ; k++ ){
-               if( sud[j][i] == sud[k+1][i] && sud[j][i] != 0 || sud[j][i] > 9 ){
-                return 0;
-               }
-            }
-        }
+    // Check the column
+    for (int i = 0; i < 9; i++) {
+        if (sud[i][col] == num) return 0;
     }
-    int x[9], m = 0;
-    for( l = 0 ; l < 3 ; l++ ){
-        for( k = 0 ; k < 3 ; k++ ){
-            for( i = 0 ; i < 3 ; i++ ){
-                for( j = 0 ; j < 3 ; j++ ){
-                    x[m] = sud[i+k*3][j+l*3];
-                    m++;
-        }
-    }
-            for( i = 0 ; i < 9 ; i++ ){
-                for( j = i ; j < 8 ; j++ ){
-                    if( x[i] == x[j+1] && x[i] != 0 || sud[i][j] > 9 ){
-                        return 0;
-                    }
-                }
-            }
-            m = 0;
+    // Check the 3x3 sub-grid
+    int startRow = row - row % 3, startCol = col - col % 3;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (sud[i + startRow][j + startCol] == num) return 0;
         }
     }
     return 1;
 }
 
-    int main(){
-    int i, j, k, l, pos = 0, count = 0, count2 = 0;
-int sud[9][9];
-for( i = 0 ; i < 9 ; i++ ){
-    for( j = 0 ; j < 9 ; j++ ){
-        scanf("%d",&sud[i][j]);
-    }
-}
-if( check(sud) == 0 ){ printf("Invalid Sudoku\n"); return 0; }
-for( i = 0 ; i < 9 ; i++ ){
-    for( j = 0 ; j < 9 ; j++ ){
-        if( sud[i][j] == 0 ){count++;}
-    }
-}
-    int y[count][2], m = 0;
-    for( i = 0 ; i < 9 ; i++ ){
-        for( j = 0 ; j < 9 ; j++ ){
-            if( sud[i][j] == 0 ){
-             y[m][0] = i;
-             y[m][1] = j;
-             m++;
+int solveSudoku(int sud[9][9]) {
+    for (int row = 0; row < 9; row++) {
+        for (int col = 0; col < 9; col++) {
+            if (sud[row][col] == 0) {
+                for (int num = 1; num <= 9; num++) {
+                    if (check(sud, row, col, num)) {
+                        sud[row][col] = num;
+                        if (solveSudoku(sud)) return 1;  // Recursively solve
+                        sud[row][col] = 0;  // Backtrack if solution not found
+                    }
+                }
+                return 0;  // No valid number found, backtrack
             }
         }
     }
-    int num = 1;
-    for( i = 0 ; i < count ; i++ ){
-            x:
-        sud[y[i][0]][y[i][1]] = num;
-        if( check(sud) == 0 && num <= 9 ){
-            num++;
-            goto x;
+    return 1;  // Puzzle solved
+}
+
+int main() {
+    int sud[9][9];
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            scanf("%d", &sud[i][j]);
         }
-        if( check(sud) == 0 && num > 9 ){
-            sud[y[i][0]][y[i][1]] = 0;
-            i--;
-            sud[y[i][0]][y[i][1]]++;
-            num = sud[y[i][0]][y[i][1]];
-            goto x;
-        }
-        num = 1;
     }
 
-     printf("\n");
-        for( i = 0 ; i < 9 ; i++ ){
-        for( j = 0 ; j < 9 ; j++ ){
-            printf("%d ",sud[i][j]);
+    if (solveSudoku(sud)) {
+        printf("\nSolved Sudoku:\n");
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                printf("%d ", sud[i][j]);
+            }
+            printf("\n");
         }
-        printf("\n");
-        }
-    return 123;
+    } else {
+        printf("No solution exists\n");
+    }
+    return 0;
 }
+
 /*
 HARDEST SAMPLE:
 
